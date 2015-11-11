2a82145ee7a3b49f6010e76d65d408b550fc9aaa
xfdingustc <xfdingyy@qq.com>
2015-11-11 17:45:07 +0800

    Add armv7a jniLib
    Differential Revision: http://review.waylens.com/D840

--- Hachi/app/src/main/java/com/transee/vdb/HttpRemuxer.java
+++ Hachi/app/src/main/java/com/transee/vdb/HttpRemuxer.java
@@ -2,104 +2,101 @@ package com.transee.vdb;
 
 import android.util.Log;
 
+import com.orhanobut.logger.Logger;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 abstract public class HttpRemuxer {
+    private static final String TAG = HttpRemuxer.class.getSimpleName();
 
-	static final boolean DEBUG = false;
-	static final String TAG = "HttpRemuxer";
+    protected static AtomicInteger mNumInstances = new AtomicInteger();
 
-	static protected AtomicInteger mNumInstances = new AtomicInteger();
+    abstract public void onEventAsync(HttpRemuxer remuxer, int event, int arg1, int arg2);
 
-	abstract public void onEventAsync(HttpRemuxer remuxer, int event, int arg1, int arg2);
+    public static final int EVENT_FINISHED = 0;
+    public static final int EVENT_ERROR = 1;
+    public static final int EVENT_PROGRESS = 2;
 
-	public static final int EVENT_FINISHED = 0;
-	public static final int EVENT_ERROR = 1;
-	public static final int EVENT_PROGRESS = 2;
+    private String mOutputFile;
+    private final int mSessionCounter;
+    private boolean mbError;
 
-	private String mOutputFile;
-	private final int mSessionCounter;
-	private boolean mbError;
+    static {
+        LibLoader.load();
+    }
 
-	static {
-		LibLoader.load();
-	}
+    public HttpRemuxer(int sessionCounter) {
+        native_init();
+        mSessionCounter = sessionCounter;
+        int num = mNumInstances.incrementAndGet();
+        Logger.t(TAG).d("create HttpRemuxer, total " + num);
+    }
 
-	public HttpRemuxer(int sessionCounter) {
-		native_init();
-		mSessionCounter = sessionCounter;
-		int num = mNumInstances.incrementAndGet();
-		if (DEBUG) {
-			Log.d(TAG, "create HttpRemuxer, total " + num);
-		}
-	}
+    public final int setIframeOnly(boolean bIframeOnly) {
+        return native_set_iframe_only(bIframeOnly);
+    }
 
-	public final int setIframeOnly(boolean bIframeOnly) {
-		return native_set_iframe_only(bIframeOnly);
-	}
+    // http://192.168.110.1:8085/clip/0/154711d3/0/360360-1001-0-0.ts,0,-1;
+    public final int run(RemuxerParams params, String outputFile) {
+        String inputFile = params.getInputFile();
+        String inputMime = params.getInputMime();
+        String outputFormat = params.getOutputFormat();
+        int duration_ms = params.getDurationMs();
+        mOutputFile = outputFile;
+        boolean bMuteAudio = params.getDisableAudio();
+        String audioFileName = params.getAudioFileName();
+        if (bMuteAudio || (audioFileName != null && audioFileName.length() > 0)) {
+            native_setAudio(bMuteAudio, audioFileName, "mp3");
+        }
+        return native_run(inputFile, inputMime, outputFile, outputFormat, duration_ms);
+    }
 
-	// http://192.168.110.1:8085/clip/0/154711d3/0/360360-1001-0-0.ts,0,-1;
-	public final int run(RemuxerParams params, String outputFile) {
-		String inputFile = params.getInputFile();
-		String inputMime = params.getInputMime();
-		String outputFormat = params.getOutputFormat();
-		int duration_ms = params.getDurationMs();
-		mOutputFile = outputFile;
-		boolean bMuteAudio = params.getDisableAudio();
-		String audioFileName = params.getAudioFileName();
-		if (bMuteAudio || (audioFileName != null && audioFileName.length() > 0)) {
-			native_setAudio(bMuteAudio, audioFileName, "mp3");
-		}
-		return native_run(inputFile, inputMime, outputFile, outputFormat, duration_ms);
-	}
+    public final String getOutputFile() {
+        return mOutputFile;
+    }
 
-	public final String getOutputFile() {
-		return mOutputFile;
-	}
+    public final int getSessionCounter() {
+        return mSessionCounter;
+    }
 
-	public final int getSessionCounter() {
-		return mSessionCounter;
-	}
+    public final void setError(boolean bError) {
+        mbError = bError;
+    }
 
-	public final void setError(boolean bError) {
-		mbError = bError;
-	}
+    public final boolean isError() {
+        return mbError;
+    }
 
-	public final boolean isError() {
-		return mbError;
-	}
+    public final void release() {
+        native_release();
+        int num = mNumInstances.decrementAndGet();
 
-	public final void release() {
-		native_release();
-		int num = mNumInstances.decrementAndGet();
-		if (DEBUG) {
-			Log.d(TAG, "release HttpRemuxer, total " + num);
-		}
-	}
+        Logger.t(TAG).d("release HttpRemuxer, total " + num);
+    }
 
-	@Override
-	protected void finalize() throws Throwable {
-		native_finalize();
-		super.finalize();
-	}
+    @Override
+    protected void finalize() throws Throwable {
+        native_finalize();
+        super.finalize();
+    }
 
-	protected void notify(int event, int arg1, int arg2) {
-		onEventAsync(this, event, arg1, arg2);
-	}
+    protected void notify(int event, int arg1, int arg2) {
+        onEventAsync(this, event, arg1, arg2);
+    }
 
-	// used by native
-	private int mNativeContext;
+    // used by native
+    private int mNativeContext;
 
-	private native void native_init();
+    private native void native_init();
 
-	private native void native_release();
+    private native void native_release();
 
-	private native int native_set_iframe_only(boolean bIframeOnly);
+    private native int native_set_iframe_only(boolean bIframeOnly);
 
-	private native int native_setAudio(boolean bDisableAudio, String audioFileName, String audioFormat);
+    private native int native_setAudio(boolean bDisableAudio, String audioFileName, String audioFormat);
 
-	private native int native_run(String inputFile, String inputMime, String outputFile, String outputFormat,
-			int duration_ms);
+    private native int native_run(String inputFile, String inputMime, String outputFile, String outputFormat,
+                                  int duration_ms);
 
-	private native void native_finalize();
+    private native void native_finalize();
 }
--- Hachi/app/src/main/java/com/transee/vdb/LibLoader.java
+++ Hachi/app/src/main/java/com/transee/vdb/LibLoader.java
@@ -1,17 +1,20 @@
 package com.transee.vdb;
 
+import com.orhanobut.logger.Logger;
+
 public class LibLoader {
+    private static final String TAG = LibLoader.class.getSimpleName();
+    static Object mLock = new Object();
+    static boolean mbLoaded = false;
 
-	static Object mLock = new Object();
-	static boolean mbLoaded = false;
-
-	static void load() {
-		synchronized (mLock) {
-			if (!mbLoaded) {
-				mbLoaded = true;
-				System.loadLibrary("avfmedia");
-			}
-		}
-	}
+    static void load() {
+        synchronized (mLock) {
+            if (!mbLoaded) {
+                mbLoaded = true;
+                Logger.t(TAG).d("LLLLLLLLLLLLLLLLLLLLLLLLLLLL");
+                System.loadLibrary("avfmedia");
+            }
+        }
+    }
 
 }
