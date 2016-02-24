f675a03dcb9eae560528dabe16702d880dbcaa44
xfdingustc <xfdingyy@qq.com>
2016-02-24 15:53:42 +0800

    refine ClipSetGroup item layout

--- Hachi/app/src/main/java/com/waylens/hachi/ui/adapters/ClipSetGroupAdapter.java
+++ Hachi/app/src/main/java/com/waylens/hachi/ui/adapters/ClipSetGroupAdapter.java
@@ -3,6 +3,7 @@ package com.waylens.hachi.ui.adapters;
 import android.content.Context;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,9 @@ import com.waylens.hachi.ui.LayoutManager.WrapGridLayoutManager;
 import com.waylens.hachi.vdb.Clip;
 import com.waylens.hachi.vdb.ClipSet;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -52,7 +56,8 @@ public class ClipSetGroupAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         ClipSet clipSet = mClipSetGroup.get(position);
         ClipSetGroupViewHolder viewHolder = (ClipSetGroupViewHolder)holder;
 
-        viewHolder.mClipSetDate.setText(clipSet.getClip(0).getDateString());
+
+        viewHolder.mClipSetDate.setText(getFormattedDate(clipSet.getClip(0).clipDate));
 
         viewHolder.mRvClipGrid.setLayoutManager(new WrapGridLayoutManager(mContext, 4));
         ClipSetGridAdapter adapter = new ClipSetGridAdapter(mContext, clipSet, new ClipSetGridAdapter.OnClipClickListener() {
@@ -80,6 +85,33 @@ public class ClipSetGroupAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         notifyDataSetChanged();
     }
 
+    private String getFormattedDate(int date) {
+        SimpleDateFormat format = new SimpleDateFormat("MMM dd,yyyy");
+
+        long clipDate = (long)date * 1000;
+        long currentTime = System.currentTimeMillis();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(clipDate);
+        int clipDateDay = calendar.get(Calendar.DAY_OF_YEAR);
+        int clipDateYear = calendar.get(Calendar.YEAR);
+
+        calendar.setTimeInMillis(currentTime);
+        int currentDateDay = calendar.get(Calendar.DAY_OF_YEAR);
+        int currentDateYear = calendar.get(Calendar.YEAR);
+
+
+        String dateString = format.format(clipDate);
+
+        if (clipDateYear == currentDateYear) {
+            if ((currentDateDay - clipDateDay) < 1) {
+                dateString = mContext.getString(R.string.today);
+            } else if ((currentDateDay - clipDateDay) < 2) {
+                dateString = mContext.getString(R.string.yesterday);
+            }
+        }
+        return dateString;
+    }
 
     public static class ClipSetGroupViewHolder extends RecyclerView.ViewHolder {
 
