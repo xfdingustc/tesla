0b6b75874f6a990f7d5f681cdc390376f5858423
xfdingustc <xfdingyy@qq.com>
2016-03-25 16:37:51 +0800

    add WelcomeActivituy

---
+++ Hachi/app/src/main/java/com/waylens/hachi/ui/activities/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.waylens.hachi.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.waylens.hachi.R;
+import com.waylens.hachi.hardware.vdtcamera.BtState;
+
+/**
+ * Created by Xiaofei on 2016/3/25.
+ */
+public class WelcomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a);
+    }
+}
