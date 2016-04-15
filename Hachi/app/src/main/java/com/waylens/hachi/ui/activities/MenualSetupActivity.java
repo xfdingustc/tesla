261dcfbc2d147b925d8e3e3ee1c9d1a0be0e54bc
xfdingustc <xfding@cam2cloud.com>
2016-04-15 11:07:24 +0800

    add camera permission request in MenualSetup

--- Hachi/app/src/main/java/com/waylens/hachi/ui/activities/MenualSetupActivity.java
+++ Hachi/app/src/main/java/com/waylens/hachi/ui/activities/MenualSetupActivity.java
@@ -1,16 +1,22 @@
 package com.waylens.hachi.ui.activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.orhanobut.logger.Logger;
 import com.waylens.hachi.R;
 import com.waylens.hachi.ui.fragments.ScanQrCodeFragment;
@@ -22,6 +28,8 @@ public class MenualSetupActivity extends BaseActivity {
     private static final String TAG = MenualSetupActivity.class.getSimpleName();
     private BroadcastReceiver mWifiStateReceiver;
     private ScanQrCodeFragment mScanQrCodeFragment;
+
+    private final int PERMISSION_REQUEST_CAMERA = 0;
 
 
     public static void launch(Activity activity) {
@@ -38,10 +46,24 @@ public class MenualSetupActivity extends BaseActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        mScanQrCodeFragment = new ScanQrCodeFragment();
-        getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, mScanQrCodeFragment).commit();
-
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            mScanQrCodeFragment = new ScanQrCodeFragment();
+            getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, mScanQrCodeFragment).commit();
+        } else {
+//            MaterialDialog dialog = new MaterialDialog.Builder(this)
+//                    .title(R.string.grant_camera_permission)
+//                    .positiveText(R.string.grant)
+//                    .negativeText(R.string.skip)
+//                    .callback(new MaterialDialog.ButtonCallback() {
+//                        @Override
+//                        public void onPositive(MaterialDialog dialog) {
+//                            super.onPositive(dialog);
+//                        }
+//                    })
+//                    .build();
+//            dialog.show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
     }
 
     @Override
@@ -53,6 +75,18 @@ public class MenualSetupActivity extends BaseActivity {
 
     private void initViews() {
         setContentView(R.layout.activity_menual_setup);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mScanQrCodeFragment = new ScanQrCodeFragment();
+                    getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, mScanQrCodeFragment).commit();
+                }
+                break;
+        }
     }
 
     @Override
