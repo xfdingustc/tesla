691dfd1b13e6e6b84bcb1715cc3de35a3fe71a20
xfdingustc <xfdingyy@qq.com>
2016-06-07 13:47:40 +0800

    set auto focus mode in Scan Qr preview

--- Hachi/app/src/main/java/com/waylens/hachi/ui/manualsetup/UltraQrCodeReaderView.java
+++ Hachi/app/src/main/java/com/waylens/hachi/ui/manualsetup/UltraQrCodeReaderView.java
@@ -1,16 +1,23 @@
 package com.waylens.hachi.ui.manualsetup;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.orhanobut.logger.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Xiaofei on 2016/5/9.
  */
 public class UltraQrCodeReaderView extends QRCodeReaderView {
+    private static final String TAG = UltraQrCodeReaderView.class.getSimpleName();
+
     public UltraQrCodeReaderView(Context context) {
         super(context);
     }
@@ -29,7 +36,20 @@ public class UltraQrCodeReaderView extends QRCodeReaderView {
 //        parameters.getAutoExposureLock();
 //        parameters.setAutoExposureLock();
 
+
+//        Rect rect = new Rect(0, 0, 500, 500);
+//        Camera.Area area = new Camera.Area(rect, 0);
+//        List<Camera.Area> focusArea = new ArrayList<>();
+//        focusArea.add(area);
+//        parameters.setFocusAreas(focusArea);
+
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+
+
         parameters.setExposureCompensation(minExposure);
         getCameraManager().getCamera().setParameters(parameters);
     }
+
+
+
 }
