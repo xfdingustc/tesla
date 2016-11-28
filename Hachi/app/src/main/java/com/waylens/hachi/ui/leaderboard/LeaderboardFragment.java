8c57cd43e82f1aab9bedf998fb2005b55c13fc49
xfdingustc <xfding@cam2cloud.com>
2016-11-28 18:01:55 +0800

    add collapse toolbar layout in leaderboard
    Change-Id: I4291c59bd11d94089e06d239973e6c6e6e94593e

--- Hachi/app/src/main/java/com/waylens/hachi/ui/leaderboard/LeaderboardFragment.java
+++ Hachi/app/src/main/java/com/waylens/hachi/ui/leaderboard/LeaderboardFragment.java
@@ -243,15 +243,8 @@ public class LeaderboardFragment extends BaseFragment implements SwipeRefreshLay
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = createFragmentView(inflater, container, R.layout.fragment_leaderboard, savedInstanceState);
-        mRvLeaderboardList.setAdapter(mAdapter);
-        mRvLeaderboardList.setLayoutManager(mLinearLayoutManager);
-        mRefreshLayout.setOnRefreshListener(this);
-        mRvLeaderboardList.setOnLoadMoreListener(new RecyclerViewExt.OnLoadMoreListener() {
-            @Override
-            public void loadMore() {
-                //loadLeaderBoard(mCurrentCursor, false);
-            }
-        });
+        setupLeaderboardItemList();
+
 
         setupLeaderBoardFilter();
 
@@ -297,6 +290,35 @@ public class LeaderboardFragment extends BaseFragment implements SwipeRefreshLay
         });
 
         return view;
+    }
+
+    private void setupLeaderboardItemList() {
+        mRvLeaderboardList.setAdapter(mAdapter);
+        mRvLeaderboardList.setLayoutManager(mLinearLayoutManager);
+        mRefreshLayout.setOnRefreshListener(this);
+        mRvLeaderboardList.setOnLoadMoreListener(new RecyclerViewExt.OnLoadMoreListener() {
+            @Override
+            public void loadMore() {
+                //loadLeaderBoard(mCurrentCursor, false);
+            }
+        });
+//        mRvLeaderboardList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+//                super.onScrollStateChanged(recyclerView, newState);
+//            }
+//
+//            @Override
+//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//            }
+//        });
+//        mRvLeaderboardList.setOnFlingListener(new RecyclerView.OnFlingListener() {
+//            @Override
+//            public boolean onFling(int velocityX, int velocityY) {
+//                return false;
+//            }
+//        });
     }
 
     private void setupLeaderBoardFilter() {
